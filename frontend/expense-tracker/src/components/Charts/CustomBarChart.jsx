14ae72b3144598e26c74c91cd3af26633a56ea49
sks032011 <skrs032011@gmail.com>
2025-07-19 01:33:15 +0530

    test

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
@@ -20,7 +20,7 @@ const CustomBarChart = ({ data }) => {
     const CustomTooltip = ({ active, payload }) => {
         if (active && payload && payload.length) {
             return (
-                <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
+                <div className="bg-white shadow-md rounded-lg p-3 border border-gray-300">
                     <p className='text-xs font-semibold text-blue-500 mb-1'>{payload[0].payload.category}</p>
                     <p className='text-sm font-medium text-gray-700'>Rs.{payload[0].value}</p>
                 </div>
